cf89de632c38ccd8478cfc21ff25971ed599dae2
fyordo <lagutinfedya@gmail.com>
2022-11-19 12:06:26 +0300

    Add route tests

---
+++ tests/Unit/AuthTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    public function testRegister(){
+        $payload = [
+            'email' => 'test@mail.ru',
+            'name' => 'TEST',
+            'password' => '123123'
+        ];
+
+        $response = $this->json('post', 'api/auth/register', $payload)->decodeResponseJson();
+
+        $this->assertEquals($response['status'], 'success');
+        $this->assertNotNull(Auth::user());
+    }
+
+    public function testLogin(){
+        $user = $this->createClientUser();
+
+        $payload = [];
+
+        $payload['email'] = $user->email;
+        $payload['password'] = '123123';
+
+        $response = $this->json('post', 'api/auth/login', $payload)->decodeResponseJson();
+
+        $this->assertEquals($response['status'], 'success');
+        $this->assertNotNull(Auth::user());
+    }
+
+    public function testLogout(){
+        $user = $this->createClientUser();
+
+        $token = Auth::login($user);
+
+        $response = $this->json('post', 'api/auth/logout', [], ['Authorization' => 'Bearer ' . $token])->decodeResponseJson();
+
+        $this->assertEquals($response['status'], 'success');
+        $this->assertNull(Auth::user());
+    }
+}
