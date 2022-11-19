16409616c3dd1128b5c4eeb1d3113817972a421e
fyordo <lagutinfedya@gmail.com>
2022-11-19 10:08:20 +0300

    Minor fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,7 +12,6 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use RefreshDatabase;
-    use DatabaseMigrations;
 
     public function createAdminUser() : User{
         return User::create([
