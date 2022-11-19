9311bd3522045969f7a2e101c9e67b3e868a3392
fyordo <lagutinfedya@gmail.com>
2022-11-19 10:49:36 +0300

    Add JWT logic

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,18 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'middleware' => 'add.json.header'
 ], function(){
-    Route::apiResource('/role', RoleController::class);
+    Route::group([
+        'prefix' => '/auth'
+    ], function(){
+        Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+        Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register');
+        Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+        Route::post('/refresh', [\App\Http\Controllers\AuthController::class, 'refresh'])->name('refresh');
+    });
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function (){
+        Route::apiResource('/role', RoleController::class);
+    });
 });
